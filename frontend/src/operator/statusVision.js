998e5a4c05159150649733fb835f64e997b1ebaf
gauriat <147273082+gauriat@users.noreply.github.com>
2024-12-17 11:23:08 +0530

    Update statusVision.js

--- frontend/src/operator/statusVision.js
+++ frontend/src/operator/statusVision.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './operator.css';
-import asrs from 'D:/gui/Code/frontend/src/operator/Viper.jpg';
+import asrs from 'C:/Users/gauri/OneDrive/Documents/IRP1/gui_smartAssemblyLine/frontend/src/operator/Viper.jpg';
 import { Link } from 'react-router-dom';
-import logo from 'D:/gui/Code/frontend/src/logo.png';
-import logo1 from 'D:/gui/Code/frontend/src/logo1.png';
+import logo from 'C:/Users/gauri/OneDrive/Documents/IRP1/gui_smartAssemblyLine/frontend/src/logo.png';
+import logo1 from 'C:/Users/gauri/OneDrive/Documents/IRP1/gui_smartAssemblyLine/frontend/src/logo1.png';
 
 function StatusVision() {
     const productionId = sessionStorage.getItem('productionId');
@@ -14,7 +14,7 @@ function StatusVision() {
     useEffect(() => {
         const fetchProductionDetails = async () => {
             try {
-                const response = await axios.get('http://localhost:8081/productionDetails', {
+                const response = await axios.get('https://gui-smartassemblyline-1.onrender.com/productionDetails', {
                     params: { machineId: 4, productionId},
                 });
                 console.log('API Response:', response.data);
